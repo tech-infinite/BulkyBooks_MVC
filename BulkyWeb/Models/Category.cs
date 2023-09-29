50895db4c3d1ccb9915ed1e47b4c22330af91aa5
Supraja Ramaraju <test.email@example.com>
2023-09-29 14:02:46 +0100

    Added solution to repository

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        // this will be the primary key in the DB table
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
